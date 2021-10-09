ccc4066fe49e41e754722a88fa21752ffe4023b5
zverev78 <9842905@gmail.com>
2021-10-09 16:19:57 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld1
+{
+    class Program
+    {
+        /// <summary>
+        /// код который выполяется в первую очередь(точка входа)
+        /// </summary>
+        /// <param name="args">параметры запуска</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет мир!"); // выводит текст на экран
+
+            Console.ReadKey();  // дает задержку до нажатия клавишы (любой) 
+                                // прописать конкретную клавишу вставив ее между скобок не получается
+
+
+
+        }
+    }
+}
